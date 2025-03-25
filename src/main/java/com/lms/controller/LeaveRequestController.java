1232e42aad349e81f4f08d6c80b6ba0c1b859d5b
Srisanjaykumar M <srisanjaykumar2000@gmail.com>
2025-03-25 23:52:00 +0530

    Updated with Controller Layer

--- src/main/java/com/lms/controller/LeaveRequestController.java
+++ src/main/java/com/lms/controller/LeaveRequestController.java
@@ -1,5 +1,48 @@
 package com.lms.controller;
 
+import com.lms.model.LeaveRequest;
+import com.lms.service.LeaveRequestService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/leave-requests")
 public class LeaveRequestController {
 
+    @Autowired
+    private LeaveRequestService leaveRequestService;
+
+    // 1. Get all leave requests
+    @GetMapping
+    public ResponseEntity<List<LeaveRequest>> getAllLeaveRequests() {
+        return ResponseEntity.ok(leaveRequestService.getAllLeaveRequests());
+    }
+
+    // 2. Get a specific leave request by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<LeaveRequest> getLeaveRequestById(@PathVariable int id) {
+        return ResponseEntity.ok(leaveRequestService.getLeaveRequestById(id));
+    }
+
+    // 3. Create a new leave request
+    @PostMapping
+    public ResponseEntity<LeaveRequest> createLeaveRequest(@RequestBody LeaveRequest leaveRequest) {
+        return ResponseEntity.ok(leaveRequestService.createLeaveRequest(leaveRequest));
+    }
+
+    // 4. Approve or Reject a leave request
+    @PutMapping("/{id}/status")
+    public ResponseEntity<LeaveRequest> updateLeaveStatus(@PathVariable int id, @RequestParam String status) {
+        return ResponseEntity.ok(leaveRequestService.updateLeaveStatus(id, status));
+    }
+
+    // 5. Delete a leave request
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteLeaveRequest(@PathVariable int id) {
+        leaveRequestService.deleteLeaveRequest(id);
+        return ResponseEntity.ok("Leave request deleted successfully");
+    }
 }
--- src/main/java/com/lms/controller/UserController.java
+++ src/main/java/com/lms/controller/UserController.java
@@ -1,5 +1,48 @@
 package com.lms.controller;
 
+import com.lms.model.User;
+import com.lms.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/users")
 public class UserController {
 
+    @Autowired
+    private UserService userService;
+
+    // 1. Get all users
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
+    }
+
+    // 2. Get user by ID
+    @GetMapping("/{id}")
+    public ResponseEntity<User> getUserById(@PathVariable int id) {
+        return ResponseEntity.ok(userService.getUserById(id));
+    }
+
+    // 3. Create a new user
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        return ResponseEntity.ok(userService.createUser(user));
+    }
+
+    // 4. Update a user
+    @PutMapping("/{id}")
+    public ResponseEntity<User> updateUser(@PathVariable int id, @RequestBody User user) {
+        return ResponseEntity.ok(userService.updateUser(id, user));
+    }
+
+    // 5. Delete a user
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteUser(@PathVariable int id) {
+        userService.deleteUser(id);
+        return ResponseEntity.ok("User deleted successfully");
+    }
 }
